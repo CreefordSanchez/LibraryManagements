482ef2991af22916c7e47e1c43513771f9372812
Myles Reid <myles@reidsmailbox.com>
2025-04-04 14:33:42 -0500

    EventReview Gets added through the tree.

--- LibraryManagement/Controllers/EventReviewController.cs
+++ LibraryManagement/Controllers/EventReviewController.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryManagement.BLL;
+using LibraryManagement.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManagement.Controllers {
-    public class EventReviewController : Controller {
-        public IActionResult Index() {
-            return View();
+    public class EventReviewController(EventReviewService service) : Controller {
+		private readonly EventReviewService _service = service;
+        public List<EventReview> GetAllEventReviews() {
+			return _service.GetAllEventReviews();
+		}
+
+        public List<EventReview> GetEventReviews(int eventId) {
+			return _service.GetReviewsByEvent(eventId);
+		}
+		public IActionResult Index() {
+            return View(GetAllEventReviews());
+        }
+
+        public IActionResult EventReview(int id) {
+            return View(GetEventReviews(id));
         }
     }
 }
