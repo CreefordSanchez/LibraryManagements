09fbb7554119c0724fb73909dc769bb9c807c02c
Myles Reid <myles@reidsmailbox.com>
2025-04-04 15:02:57 -0500

    Book gets

--- LibraryManagement/Controllers/BookController.cs
+++ LibraryManagement/Controllers/BookController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryManagement.BLL;
+using LibraryManagement.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManagement.Controllers {
-    public class BookController : Controller {
-        public IActionResult Index() {
-            return View();
-        }
-    }
+	public class BookController(BookService service) : Controller {
+		private readonly BookService _service = service;
+
+		public IActionResult Index() {
+			return View(_service.GetAllBooks());
+		}
+
+		public IActionResult Book(int id) {
+			try {
+				Book selected = _service.GetBook(id);
+				return View(selected);
+			} catch (KeyNotFoundException ex) {
+				return NotFound(ex.Message);
+			}
+		}
+	}
 }
