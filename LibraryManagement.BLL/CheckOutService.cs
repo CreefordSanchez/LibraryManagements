e6de9f517e7a691d88f89069ce70acf458759962
Myles Reid <myles@reidsmailbox.com>
2025-04-04 14:57:32 -0500

    CheckOut Gets

--- LibraryManagement.BLL/CheckOutService.cs
+++ LibraryManagement.BLL/CheckOutService.cs
@@ -1,5 +1,26 @@
-﻿namespace LibraryManagement.BLL {
-    public class CheckOutService {
-    }
+﻿using LibraryManagement.DAL;
+using LibraryManagement.Models;
+
+namespace LibraryManagement.BLL {
+	public class CheckOutService(CheckOutRepository repo) {
+		private readonly CheckOutRepository _repo = repo;
+
+		public List<CheckOut> GetAllCheckOuts() {
+			return _repo.GetAllCheckOuts();
+		}
+		public List<CheckOut> GetCheckOutByUser(string id) {
+			List<CheckOut>? selected = _repo.GetCheckOutByUser(id);
+			if (selected == null) {
+				throw new KeyNotFoundException($"No CheckOuts found with User Id being {id}");
+			}
+			return selected;
+		}
+		public List<CheckOut> GetCheckOutByDueDate(DateTime dueDate) {
+			List<CheckOut> selected = _repo.GetCheckOutByDueDate(dueDate);
+			if (selected == null || selected.Count == 0) {
+				throw new KeyNotFoundException("No CheckOuts found for the given due date");
+			}
+			return selected;
+		}
+	}
 }
- 
--- LibraryManagement.DAL/CheckOutRepository.cs
+++ LibraryManagement.DAL/CheckOutRepository.cs
@@ -1,4 +1,19 @@
-﻿namespace LibraryManagement.DAL {
-    public class CheckOutRepository {
-    }
+﻿using LibraryManagement.Models;
+
+namespace LibraryManagement.DAL {
+	public class CheckOutRepository(LibraryManagementContext context) {
+		private readonly LibraryManagementContext _context = context;
+
+		public List<CheckOut> GetAllCheckOuts() {
+			return _context.CheckOuts.ToList();
+		}
+
+		public List<CheckOut> GetCheckOutByUser(string id) {
+			return _context.CheckOuts.Where(x => x.UserId == id).ToList();
+		}
+
+		public List<CheckOut> GetCheckOutByDueDate(DateTime dueDate) {
+			return _context.CheckOuts.Where(x => x.DueDate == dueDate).ToList();
+		}
+	}
 }
