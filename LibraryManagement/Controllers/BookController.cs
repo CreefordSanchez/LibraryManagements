c11e4b2b826c778fecdf4077eee23037a6ababb3
Myles Reid <myles@reidsmailbox.com>
2025-04-14 13:12:43 -0500

    Individual book view.

--- LibraryManagement/Controllers/BookController.cs
+++ LibraryManagement/Controllers/BookController.cs
@@ -3,14 +3,16 @@ using LibraryManagement.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManagement.Controllers {
-	public class BookController(BookService service) : Controller {
+	public class BookController(BookService service, BookReviewService rService) : Controller {
 		private readonly BookService _service = service;
+		private readonly BookReviewService _reviewService = rService;
 
 		public IActionResult Index() {
 			return View(_service.GetAllBooks());
 		}
 
 		public IActionResult Book(int id) {
+			ViewBag.ReviewList = _reviewService.GetReviewsByBook(id);
 			return View(_service.GetBook(id));
 		}
 
