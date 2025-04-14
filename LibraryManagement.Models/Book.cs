0da80975997940ae7c6df7eb334fef32fdd13e41
Creeford Sanchez <creewrk@gmail.com>
2025-04-14 13:06:11 -0700

    To DateOnly for Book/CheckOut model

--- LibraryManagement.Models/Book.cs
+++ LibraryManagement.Models/Book.cs
@@ -6,7 +6,7 @@ namespace LibraryManagement.Models {
         public string Title { get; set; }
         public string Author { get; set; }
         public string Genre { get; set; }
-        public DateTime Published { get; set; }
+        public DateOnly Published { get; set; }
         public ICollection<BookReview>? BookReviews { get; set; }
         public CheckOut? CheckOut { get; set; }
     }
--- LibraryManagement.Models/CheckOut.cs
+++ LibraryManagement.Models/CheckOut.cs
@@ -5,21 +5,8 @@
 		public string UserId { get; set; }
 		public bool IsReturned { get; set; }
 		public bool IsOverdue { get; set; }
-		public DateTime DueDate { get; set; }
-		public DateTime CheckoutDate { get; set; }
-		public bool AuthorizeCheckout { get; set; }
-
-		// These may be able to be removed and we can alter DueDate and CheckoutDate to be DateOnly
-		public DateOnly GetDueDate() {
-			return DateOnly.FromDateTime(this.DueDate);
-		}
-
-		public DateOnly GetCheckoutDate() {
-			return DateOnly.FromDateTime(this.CheckoutDate);
-
-
-		public static DateOnly GetToday() {
-			return DateOnly.FromDateTime(DateTime.Now);
-		}
+		public DateOnly DueDate { get; set; }
+		public DateOnly CheckoutDate { get; set; }
+		public bool AuthorizeCheckout { get; set; }	
 	}
 }
