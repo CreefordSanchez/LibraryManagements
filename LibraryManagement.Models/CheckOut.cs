fb0a141c7b030dca1d8f9578e317ee5a8eed5f2d
Myles Reid <myles@reidsmailbox.com>
2025-04-14 11:27:27 -0500

    Checkout Index.
    Added methods for Getting Date only checkout and duedate

--- LibraryManagement.Models/CheckOut.cs
+++ LibraryManagement.Models/CheckOut.cs
@@ -1,19 +1,24 @@
 ﻿namespace LibraryManagement.Models {
-    public class CheckOut {
-        public int BookId { get; set; }
-        public Book? Book { get; set; }
-        public string UserId { get; set; }
-        public bool IsReturned { get; set; }
-        public bool IsOverdue { get; set; }
-        public DateTime DueDate { get; set; }
-        public DateTime CheckoutDate { get; set; }
-        public bool AuthorizeCheckout { get; set; }
+	public class CheckOut {
+		public int BookId { get; set; }
+		public Book? Book { get; set; }
+		public string UserId { get; set; }
+		public bool IsReturned { get; set; }
+		public bool IsOverdue { get; set; }
+		public DateTime DueDate { get; set; }
+		public DateTime CheckoutDate { get; set; }
+		public bool AuthorizeCheckout { get; set; }
 
-        public DateOnly GetDueDate() {
-            return DateOnly.FromDateTime(this.DueDate);
+		// These may be able to be removed and we can alter DueDate and CheckoutDate to be DateOnly
+		public DateOnly GetDueDate() {
+			return DateOnly.FromDateTime(this.DueDate);
 		}
 
-        public static DateOnly GetToday() {
+		public DateOnly GetCheckoutDate() {
+			return DateOnly.FromDateTime(this.CheckoutDate);
+
+
+		public static DateOnly GetToday() {
 			return DateOnly.FromDateTime(DateTime.Now);
 		}
 	}
