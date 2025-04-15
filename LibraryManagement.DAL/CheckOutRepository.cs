49775b8dd2ce98ac6d8def22f08d2bfe764bb935
Myles Reid <myles@reidsmailbox.com>
2025-04-15 08:58:33 -0500

    Added Book reference to CheckOuts

--- LibraryManagement.DAL/CheckOutRepository.cs
+++ LibraryManagement.DAL/CheckOutRepository.cs
@@ -1,19 +1,20 @@
 ﻿using LibraryManagement.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryManagement.DAL {
 	public class CheckOutRepository(LibraryManagementContext context) {
 		private readonly LibraryManagementContext _context = context;
 
 		public List<CheckOut> GetAllCheckOuts() {
-			return _context.CheckOuts.ToList();
+			return _context.CheckOuts.Include(c => c.Book).ToList();
 		}
 
 		public List<CheckOut> GetCheckOutByUser(string id) {
-			return _context.CheckOuts.Where(x => x.UserId == id).ToList();
+			return _context.CheckOuts.Where(x => x.UserId == id).Include(c => c.Book).ToList();
 		}
 
 		public List<CheckOut> GetCheckOutByDueDate(DateOnly dueDate) {
-			return _context.CheckOuts.Where(x => x.DueDate == dueDate).ToList();
+			return _context.CheckOuts.Where(x => x.DueDate == dueDate).Include(c => c.Book).ToList();
 		}
 
 		public void CreateCheckOut(CheckOut check) {
