a60327f721c6b1f7aef2ecf3e9413dac147a8d66
Myles Reid <myles@reidsmailbox.com>
2025-04-15 12:24:41 -0500

    Added home page

---
+++ LibraryManagement.Models/ModelViews/BookAndEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Models.ModelViews
+{
+    public class BookAndEvent
+    {
+		public int BookId { get; set; }
+		public string BookTitle { get; set; }
+		public string BookAuthor { get; set; }
+        public DateOnly BookPublished { get; set; }
+		public string BookGenre { get; set; }
+
+		public int EventId { get; set; }
+		public string EventTitle { get; set; }
+        public DateOnly EventDate { get; set; }
+        public TimeOnly EventTime { get; set; }
+		public string EventLocation { get; set; }
+	}
+}
